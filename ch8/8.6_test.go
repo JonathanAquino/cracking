7fee14d9af103f93c1a55064411fb856d3d3ecd7
Jon Aquino <jona@ning.com>
2021-02-10 00:26:05 -0800

    Add question 8.6

--- ch8/8.6_test.go
+++ ch8/8.6_test.go
@@ -12,50 +12,64 @@ import (
 // The classic Towers of Hanoi problem.
 
 // Moves count disks from the stack named from to the stack named to.
-func move(from, to, other string, count int, stacks map[string]*Stack) {
-	println(fmt.Sprintf("move(%s, %s, %s, %d)", from, to, other, count))
+func move(from, to, other string, count int, stacks map[string]*Stack, indent int) {
+	println(spaces(indent) + fmt.Sprintf("move(%s, %s, %s, %d)", from, to, other, count))
 	if count == 0 {
 		return
 	}
 	if count == 1 {
-		if !stacks[to].isEmpty() && stacks[to].peek() < stacks[from].peek() {
-			move(from, other, to, 1, stacks)
-			move(to, from, other, 1, stacks)
-			move(other, to, from, 1, stacks)
-			move(from, to, other, 1, stacks)
+		if stacks[to].isEmpty() || stacks[to].peek() > stacks[from].peek() {
+			item := stacks[from].pop()
+			stacks[to].push(item)
+			printStacks(stacks, indent)
 			return
 		}
-		item := stacks[from].pop()
-		stacks[to].push(item)
-		printStacks(stacks)
-		return
 	}
-	move(from, other, to, 1, stacks)
-	toDisksToMove := stacks[to].size()
-	if !stacks[to].isEmpty() && stacks[to].peek() > stacks[from].peek() {
-		toDisksToMove = 0
+	fromDisk := stacks[from].peek()
+	numSmallerToDisks := numSmaller(stacks[to], fromDisk)
+	move(from, other, to, 1, stacks, indent+1)
+	move(to, from, other, numSmallerToDisks, stacks, indent+1)
+	move(other, to, from, 1, stacks, indent+1)
+	move(from, to, other, numSmallerToDisks, stacks, indent+1)
+	move(from, to, other, count-1, stacks, indent+1)
+}
+
+// Returns the number of disks that are smaller than n.
+func numSmaller(stack *Stack, n int) int {
+	num := 0
+	for i := stack.size() - 1; i >= 0; i-- {
+		if stack.items[i] >= n {
+			break
+		}
+		num++
 	}
-	move(to, from, other, toDisksToMove, stacks)
-	move(other, to, from, 1, stacks)
-	move(from, to, other, toDisksToMove, stacks)
-	move(from, to, other, stacks[from].size(), stacks)
+	return num
 }
 
 // Outputs the contents of the stacks named a, b, and c.
-func printStacks(stacks map[string]*Stack) {
-	printStack(stacks["a"])
-	printStack(stacks["b"])
-	printStack(stacks["c"])
+func printStacks(stacks map[string]*Stack, indent int) {
+	printStack(stacks["a"], indent)
+	printStack(stacks["b"], indent)
+	printStack(stacks["c"], indent)
 	println()
 }
 
 // Outputs the contents of the stack
-func printStack(stack *Stack) {
+func printStack(stack *Stack, indent int) {
 	itemStrings := []string{}
 	for _, item := range stack.items {
 		itemStrings = append(itemStrings, strconv.Itoa(item))
 	}
-	println(">" + strings.Join(itemStrings, ","))
+	println(spaces(indent) + ">" + strings.Join(itemStrings, ","))
+}
+
+// spaces returns several spaces depending on the given indentation level.
+func spaces(indent int) string {
+	result := ""
+	for i := 0; i < indent; i++ {
+		result += " "
+	}
+	return result
 }
 
 type Stack struct {
@@ -100,14 +114,23 @@ func Test8Dot6Stack(t *testing.T) {
 	assert.True(t, s.isEmpty())
 }
 
+func Test8Dot6NumSmaller(t *testing.T) {
+	s := Stack{}
+	s.push(5)
+	s.push(4)
+	assert.Equal(t, 2, numSmaller(&s, 6))
+	assert.Equal(t, 1, numSmaller(&s, 5))
+	assert.Equal(t, 0, numSmaller(&s, 4))
+}
+
 func Test8Dot6Move(t *testing.T) {
 	stacks := map[string]*Stack{
 		"a": &Stack{items: []int{8, 7, 6, 5, 4, 3, 2, 1}},
 		"b": &Stack{items: []int{}},
 		"c": &Stack{items: []int{}},
 	}
-	printStacks(stacks)
-	move("a", "c", "b", stacks["a"].size(), stacks)
+	printStacks(stacks, 0)
+	move("a", "c", "b", stacks["a"].size(), stacks, 0)
 	expected := map[string]*Stack{
 		"a": &Stack{items: []int{}},
 		"b": &Stack{items: []int{}},
